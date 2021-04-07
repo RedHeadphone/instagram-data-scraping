5e2d693797cb1da2b5132b0b8b896703afe9f4e8
RedHeadphone <khilawala92@gmail.com>
2021-04-07 21:07:34 +0530

    doing

---
+++ getdata.py
@@ -0,0 +1,26 @@
+from instaloader import Instaloader, Profile
+import pandas as pd
+
+df=pd.DataFrame([["username","contact","bio"]])
+
+loader = Instaloader()
+data = 10
+users = {}
+
+def get_hashtags_posts(query):
+    posts = loader.get_hashtag_posts(query)
+    count = 0
+    for post in posts:
+        profile = post.owner_profile
+        if profile.username not in users and ("India" in profile.biography):  #  or post.location
+            users[profile.username]=True
+            df.append([profile.username,profile.external_url,profile.biography])
+            count += 1
+            print('{}: {}'.format(count, profile.username))
+            if count == data:
+                break
+
+if __name__ == "__main__":
+    hashtag = "clothes"
+    get_hashtags_posts(hashtag)
+    df.to_csv(index=False)
