d67c31df3a9416a2de68712b2b5ea52b822b87d3
RedHeadphone <khilawala92@gmail.com>
2021-04-09 17:49:19 +0530

    done

--- getdata.py
+++ getdata.py
@@ -50,12 +50,12 @@ async def checkprofile(profile,country):
         db.collection(u'cloth').document(profile.username).set({"username":profile.username,"contact":profile.external_url,"bio":profile.biography})
         count += 1
         print('{}: {}'.format(count, profile.username))
-        c=0
-        for i in profile.get_similar_accounts():
-            asyncio.create_task(checkprofile(i,""))
-            c+=1
-            if c==5:
-                break
+        # c=0
+        # for i in profile.get_similar_accounts():
+        #     asyncio.create_task(checkprofile(i,""))
+        #     c+=1
+        #     if c==5:
+        #         break
         if count == data:
             exit()
 
