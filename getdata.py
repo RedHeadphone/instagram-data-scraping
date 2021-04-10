b24c954e330afd3c489457ff133c744187807576
RedHeadphone <khilawala92@gmail.com>
2021-04-10 13:24:55 +0530

    done ig

--- getdata.py
+++ getdata.py
@@ -1,5 +1,4 @@
 from instaloader import Instaloader, Profile
-# import pandas as pd
 from dotenv import load_dotenv
 import os,asyncio
 from geopy.geocoders import Nominatim
@@ -27,14 +26,13 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-# df=pd.DataFrame([["username","contact","bio"]])
-
 loader = Instaloader()
 loader.login(os.getenv("IGUSER"),os.getenv("IGPASSWORD"))
 data = 5000
 users = {}
 users_ref = db.collection(u'cloth')
 docs = users_ref.stream()
+sim=[]
 
 for doc in docs:
     users[doc.id]=True
@@ -42,25 +40,45 @@ count = len(users.keys())
 
 print(count)
 
+async def simpro():
+    global hashtags
+    while True:
+        if len(sim)==0:
+            asyncio.sleep(5)
+        else:
+            a=sim.pop(0)
+            k=0
+            don=False
+            for post in a.get_posts():
+                for h in post.caption_hashtags:
+                    if h in hashtags:
+                        asyncio.create_task(checkprofile(a,""))
+                        don=True
+                        break
+                if don:
+                    break
+                k+=1
+                if k==5:
+                    break
+
+
 async def checkprofile(profile,country):
     global count
     if profile.username not in users and ("India" in profile.biography or country=="India") : 
         users[profile.username]=True
-        # df=df.append([[profile.username,profile.external_url,profile.biography]])
         db.collection(u'cloth').document(profile.username).set({"username":profile.username,"contact":profile.external_url,"bio":profile.biography})
         count += 1
         print('{}: {}'.format(count, profile.username))
-        # c=0
-        # for i in profile.get_similar_accounts():
-        #     asyncio.create_task(checkprofile(i,""))
-        #     c+=1
-        #     if c==5:
-        #         break
+        c=0
+        for i in profile.get_similar_accounts():
+            sim.append(i)
+            c+=1
+            if c==10:
+                break
         if count == data:
             exit()
 
 async def get_hashtags_posts(query):
-    # global df
     posts = loader.get_hashtag_posts(query)
     for post in posts:
         profile = post.owner_profile
@@ -76,17 +94,18 @@ async def get_hashtags_posts(query):
         except:
             country=""
         await checkprofile(profile,country)
-        
+
+hashtags = ["clothes",  "clothing",  "clothingbrand",  "cloth",  "clothesforsale",  "cloths",
+   "clothings","clothingbrands","clothesline", "clothingsale"]
 
 async def main():
-   # hashtag = "cloths"
-    hashtags = ["clothes",  "clothing",  "clothingline",  "clothingbrand",  "cloth",  "clothesforsale",  "CLOTHINGSTORE","clothingcompany",  "cloths",
-   "clothings",   "clothingforsale", "clothingbrands","clothesline", "clothingsale", "clothingco", "fashion"]
+    global hashtags
     do=True
     while do:
         try:
             for hashtag in hashtags:
                 last=asyncio.create_task(get_hashtags_posts(hashtag))
+            asyncio.create_task(simpro())
             await last
         except:
             asyncio.sleep(10)
