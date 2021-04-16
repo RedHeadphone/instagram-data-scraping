9d94d5a475a669cc9db061ff27782dddc2b62122
RedHeadphone <khilawala92@gmail.com>
2021-04-16 16:22:19 +0530

    may fix

--- getfolowers.py
+++ getfolowers.py
@@ -9,11 +9,16 @@ from instaloader import Instaloader, Profile
 loader = Instaloader()
 loader.login(os.getenv("IGUSER"),os.getenv("IGPASSWORD"))
 data=pd.read_csv('data.csv')
+#data=pd.read_csv('data-cloth.csv')
 
 c=0
 for index, row in data.iterrows():
-    if np.isnan(data.loc[index,'followers']):
-        data.loc[index,"followers"]= Profile.from_username(loader.context, data.loc[index,"username"]).followers
+    try:
+        if np.isnan(data.loc[index,'followers']):
+            data.loc[index,"followers"]= Profile.from_username(loader.context, data.loc[index,"username"]).followers
+    except:
+        pass
+    print(c)
 
-
+print("finish")
 data.to_csv("data-cloth.csv")
